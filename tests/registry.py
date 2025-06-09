be03aac5e1f695ee3402693eb4411bfd2cc93eab
Dominic Fox <dominic.fox@gmail.com>
2025-06-09 21:22:48 +0100

    First test

--- tests/registry.py
+++ tests/registry.py
@@ -2,24 +2,21 @@ import unittest
 import uuid
 from typing import Callable
 
-from versatile.registry import provides
-
-
-
+from versatile.registry import provides, components_registered_in
 
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         context = str(uuid.uuid4())
 
-        @provides(context=context)
+        @provides(name='greeter', context=context)
         def build_greeter() -> Callable:
             def greeter():
                 return 'Hello, world!'
 
             return greeter
 
-        self.assertEqual(True, False)  # add assertion here
+        self.assertEqual('greeter', components_registered_in(context)[0].name)
 
 
 if __name__ == '__main__':
